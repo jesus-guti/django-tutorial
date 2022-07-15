932d9ea230ac60f3870acec70cd53a5a70ca4816
jesus-guti <jesusgutierrezsiliceo@gmail.com>
2022-07-15 10:57:04 +0200

    configure app and set simple http response

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(resquest):
+    return HttpResponse('Home page')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home)
 ]
