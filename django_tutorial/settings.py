e7b0ab156ede5e4d454e3dbf36dfc8a1e8f053d9
jesus-guti <jesusgutierrezsiliceo@gmail.com>
2022-07-18 16:55:45 +0200

    rest_framwork settings

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -18,17 +18,28 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'base.apps.BaseConfig',
+    
+    'api.apps.APIConfig',
     'frontend.apps.FrontendConfig'
 ]
+
+PROJECT_APPS = [  
+    'api.apps.APIConfig',
+]
+
+THIRD_PARTY_APPS = [
+    "rest_framework"
+]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
