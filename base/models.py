b64dd46f0a5ca313062ab8cfabc060191aa65fd1
jesus-guti <jesusgutierrezsiliceo@gmail.com>
2022-07-18 10:01:27 +0200

    first model and room to admin

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
+from pydoc import describe
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    # host = 
+    # topic = 
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants = 
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
