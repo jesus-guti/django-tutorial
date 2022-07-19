bb062661586407376d584519a4e0a491405f35d9
jesus-guti <jesusgutierrezsiliceo@gmail.com>
2022-07-19 18:07:30 +0200

    babel install

--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -1,5 +1,6 @@
 import { React, useEffect, useState} from 'react'
 import { useParams } from "react-router-dom";
+import Navbar from '../components/Navbar';
 
 const Room = () => {
     let params = useParams();
@@ -9,16 +10,20 @@ const Room = () => {
         let response = await fetch(`/api/rooms/${params.id}`)
         let data = await response.json()
         setRoom(data)
-    }
+      }
 
-    useEffect(() => {
-     getRoom();
-    },[])
+      useEffect(() => {
+        getRoom();
+      },[])
 
-  return (
-    <div><h1>{room.name}</h1>
-    </div>
-  )
+      return (
+        <div>
+          <Navbar />
+          <main>
+            <h5>{room.topic.name}</h5>
+          </main>
+        </div>
+      )
 }
 
 export default Room
